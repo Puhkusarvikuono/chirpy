c9d0ae8679916396cc36419c89ef118976204c3d
Puhkusarvikuono <tossu18@proton.me>
2026-04-25 13:21:43 +0300

    Simple server

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
-func main(){
-	fmt.Println("Hello, this is Chirpy")
+func main() {
+	const port = "8080"
+	mux := http.NewServeMux()
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
