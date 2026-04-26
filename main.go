c769eee978992bf0e833c46e90da3483b1acf830
Puhkusarvikuono <tossu18@proton.me>
2026-04-26 11:53:35 +0300

    added basic handlers

--- main.go
+++ main.go
@@ -1,13 +1,36 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const port = "8080"
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+
+	mux.HandleFunc("/healthz/", handlerReadiness)
+
+	mux.HandleFunc("/reset/", apiCfg.handlerReset)
+
+	mux.HandleFunc("/metrics/", apiCfg.handlerMetrics)
+
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
@@ -15,4 +38,24 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	s := fmt.Sprintf("Hits: %d\n", cfg.fileserverHits.Load())
+	io.WriteString(w, s)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	io.WriteString(w, "File server hits resetted.")
 }
